767852854857db84bcd401d3d198899cda2006b2
ANANTIA TEMGOUA <elvado.temgoua@gmail.com>
2025-10-28 10:16:51 +0100

    creating user model

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,6 +3,7 @@ import { Router } from "express";
 const authRouter = Router();
 
 authRouter.post("/signup", (req, res) => res.send({ title: "Sign up " }));
+
 authRouter.post("/login", (req, res) => res.send({ title: "Log in " }));
 
 export default authRouter;
